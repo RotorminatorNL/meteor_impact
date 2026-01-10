eeeb11421fc7c41157fc14ed26c854dd3b895bb8
Robbert Lengton <robbert.lengton@hotmail.nl>
2026-01-10 15:08:49 +0100

    Haptic impulse based on distance works

--- development/Assets/_MyStuff/Script/HapticHandler.cs
+++ development/Assets/_MyStuff/Script/HapticHandler.cs
@@ -3,14 +3,24 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class HapticHandler : MonoBehaviour
 {
+    [SerializeField] private float maxDistance = 0.5f;
     [SerializeField] private Transform hapticSender;
     [SerializeField] private HapticImpulsePlayer leftController;
     [SerializeField] private HapticImpulsePlayer rightController;
 
     void Update()
     {
-        float leftDistance = Vector3.Distance(leftController.transform.position, hapticSender.position);
-        float rightDistance = Vector3.Distance(rightController.transform.position, hapticSender.position);
-        Debug.Log($"Left distance: {leftDistance} | Right distance: {rightDistance}");
+        ControllerHaptic(leftController, Vector3.Distance(leftController.transform.position, hapticSender.position));
+        ControllerHaptic(rightController, Vector3.Distance(rightController.transform.position, hapticSender.position));
+    }
+
+    private void ControllerHaptic(HapticImpulsePlayer controller, float distance)
+    {
+        if (distance < maxDistance)
+        {
+            controller.SendHapticImpulse(Mathf.InverseLerp(maxDistance, 0, distance), 0, 300);
+            return;
+        }
+        controller.SendHapticImpulse(0, 0, 0);
     }
 }

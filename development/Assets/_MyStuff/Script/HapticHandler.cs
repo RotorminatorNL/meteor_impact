b3ee830121165d02340ca7f4c3a4ef1f32dc375c
Robbert Lengton <robbert.lengton@hotmail.nl>
2026-01-12 18:33:43 +0100

    No freq manipulation for haptic

--- development/Assets/_MyStuff/Script/HapticHandler.cs
+++ development/Assets/_MyStuff/Script/HapticHandler.cs
@@ -1,26 +1,20 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class HapticHandler : MonoBehaviour
 {
-    [SerializeField] private float maxDistance = 0.5f;
     [SerializeField] private Transform hapticSender;
     [SerializeField] private HapticImpulsePlayer leftController;
     [SerializeField] private HapticImpulsePlayer rightController;
 
-    void Update()
-    {
-        ControllerHaptic(leftController, Vector3.Distance(leftController.transform.position, hapticSender.position));
-        ControllerHaptic(rightController, Vector3.Distance(rightController.transform.position, hapticSender.position));
-    }
+    private float currentAmplify = 0.2f;
+    private float currentDuration = 0.8f;
 
-    private void ControllerHaptic(HapticImpulsePlayer controller, float distance)
+    private void Start()
     {
-        if (distance < maxDistance)
-        {
-            controller.SendHapticImpulse(Mathf.InverseLerp(maxDistance, 0, distance), 0, 300);
-            return;
-        }
-        controller.SendHapticImpulse(0, 0, 0);
+        leftController.SendHapticImpulse(currentAmplify, currentDuration);
+        rightController.SendHapticImpulse(currentAmplify, currentDuration);
     }
 }

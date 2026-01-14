63b42a0bede13e599f86e753d3530d8fb2150759
Robbert Lengton <robbert.lengton@hotmail.nl>
2026-01-14 18:44:02 +0100

    Add anim to control impulse | needs improvement

--- development/Assets/_MyStuff/Script/HapticHandler.cs
+++ development/Assets/_MyStuff/Script/HapticHandler.cs
@@ -1,20 +1,46 @@
-using System.Collections;
-using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class HapticHandler : MonoBehaviour
 {
-    [SerializeField] private Transform hapticSender;
     [SerializeField] private HapticImpulsePlayer leftController;
     [SerializeField] private HapticImpulsePlayer rightController;
+    [SerializeField] private float hapticRndRange = 0.3f;
+    [SerializeField, Range(0, 1)] private float hapticAmplitude;
 
-    private float currentAmplify = 0.2f;
-    private float currentDuration = 0.8f;
+    private bool hapticOn = false;
 
-    private void Start()
+    private void Update()
     {
-        leftController.SendHapticImpulse(currentAmplify, currentDuration);
-        rightController.SendHapticImpulse(currentAmplify, currentDuration);
+        if (!hapticOn) return;
+
+        float minRndRange = hapticAmplitude - hapticRndRange;
+        float maxRndRange = hapticAmplitude + hapticRndRange;
+
+        float rndImpulseLeft = Random.Range(minRndRange, maxRndRange);
+        if (rndImpulseLeft < 0) rndImpulseLeft = 0;
+        if (rndImpulseLeft > 1) rndImpulseLeft = 1;
+        float rndImpulseRight = Random.Range(minRndRange, maxRndRange);
+        if (rndImpulseRight < 0) rndImpulseRight = 0;
+        if (rndImpulseRight > 1) rndImpulseRight = 1;
+        leftController.SendHapticImpulse(rndImpulseLeft, 0);
+        rightController.SendHapticImpulse(rndImpulseRight, 0);
+    }
+
+    public void StartHaptic()
+    {
+        hapticOn = true;
+    }
+
+    public void ChangeHapticImpulseMaxRndRange(float amount)
+    {
+        hapticRndRange = amount;
+    }
+
+    public void StopHaptic()
+    {
+        hapticOn = false;
+        leftController.SendHapticImpulse(0, 0);
+        rightController.SendHapticImpulse(0, 0);
     }
 }

39d6d2e83d5c561688bca4c2cc78c015d037d902
Robbert Lengton <robbert.lengton@hotmail.nl>
2026-01-20 05:17:43 +0100

    Start of haptic implementation

--- development/Assets/_MyStuff/Script/MeteorHandler.cs
+++ development/Assets/_MyStuff/Script/MeteorHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class MeteorHandler : MonoBehaviour
 {
@@ -10,18 +11,39 @@ public class MeteorHandler : MonoBehaviour
     [SerializeField] private AudioSource meteorAudio;
     [SerializeField] private AudioClip meteorIncoming;
     [SerializeField] private AudioClip meteorHit;
-    [SerializeField, Range(0,1)] private float spatialBlend = 1;
+    [SerializeField, Range(0, 1)] private float spatialBlend = 1;
+    [Header("Tactile")]
+    [SerializeField] private HapticImpulsePlayer leftController;
+    [SerializeField] private HapticImpulsePlayer rightController;
+    [SerializeField, Range(0, 1)] private float hapticAmplitude;
+    [SerializeField, Range(0, 1)] private float hapticRndRange;
 
     private bool useSound = false;
+    private bool useTactile = false;
 
     private void Update()
     {
-        meteorAudio.spatialBlend = spatialBlend;
+        if (!useSound) meteorAudio.spatialBlend = spatialBlend;
+        if (!useTactile)
+        {
+            float minRndRange = hapticAmplitude - hapticRndRange;
+            float maxRndRange = hapticAmplitude + hapticRndRange;
+
+            float rndImpulseLeft = Random.Range(minRndRange, maxRndRange);
+            if (rndImpulseLeft < 0) rndImpulseLeft = 0;
+            if (rndImpulseLeft > 1) rndImpulseLeft = 1;
+            float rndImpulseRight = Random.Range(minRndRange, maxRndRange);
+            if (rndImpulseRight < 0) rndImpulseRight = 0;
+            if (rndImpulseRight > 1) rndImpulseRight = 1;
+            leftController.SendHapticImpulse(rndImpulseLeft, 0);
+            rightController.SendHapticImpulse(rndImpulseRight, 0);
+        }
     }
 
     public IEnumerator StartMeteor(int chosenExperience)
     {
         useSound = false;
+        useTactile = false;
         meteorImpactController.SetBool("Active", false);
         meteorSpinningController.SetBool("Active", false);
         meteorAudio.Stop();
@@ -29,6 +51,7 @@ public class MeteorHandler : MonoBehaviour
         meteorImpactController.SetBool("Active", true);
         meteorSpinningController.SetBool("Active", true);
         if (chosenExperience == 1 || chosenExperience == 3) UseMeteorSound();
+        if (chosenExperience == 1 || chosenExperience == 4) useTactile = true;
     }
 
     public void MeteorHit()
